295793a928d935baad2760e03ba95a82f7279029
arige <417923140@qq.com>
2019-03-04 13:45:04 +0800

    【新增】新增版本文件

---
+++ mediation/src/main/java/com/mintegral/mediation/common/CommonConfig.java
@@ -0,0 +1,9 @@
+package com.mintegral.mediation.common;
+
+/**
+ * @author songjunjun
+ * @date 2019/3/4
+ **/
+public class CommonConfig {
+    public final static String PLUGIN_VERSION = "1.0.0";
+}
